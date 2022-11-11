2cdb68065a37c447b477af2b9295134fc398981f
Lior Mathan <Lior.mathan@gmail.com>
2022-11-11 16:13:53 +0200

    Added: error message in bad http responses.

--- AuthenticationSystem/src/main/java/AuthApp/controller/AuthenticationController.java
+++ AuthenticationSystem/src/main/java/AuthApp/controller/AuthenticationController.java
@@ -3,6 +3,7 @@ package AuthApp.controller;
 import AuthApp.service.AuthenticationService;
 import AuthApp.service.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,15 +17,15 @@ public class AuthenticationController {
     }
 
     @RequestMapping(value = "register", method = RequestMethod.POST)
-    public ResponseEntity<User> register(@RequestBody User user) {
+    public ResponseEntity register(@RequestBody User user) {
         if (!InputValidation.isValidEmail(user.getEmail())) {
-            throw new IllegalArgumentException("Invalid email address!");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid email address!");
         }
         if (!InputValidation.isValidName(user.getName())) {
-            throw new IllegalArgumentException("Invalid name!");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid name!");
         }
         if (!InputValidation.isValidPassword(user.getPassword())) {
-            throw new IllegalArgumentException("Invalid password!");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid password!");
         }
 
         return ResponseEntity.ok(this.authenticationService
@@ -34,7 +35,7 @@ public class AuthenticationController {
     @RequestMapping(value = "login", method = RequestMethod.POST)
     public ResponseEntity<String> login(@RequestParam String email, @RequestParam String password) {
         if (!InputValidation.isValidEmail(email)) {
-            throw new IllegalArgumentException("Your email address is invalid!");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Your email address is invalid!");
         }
 
         return ResponseEntity.ok(this.authenticationService.login(email, password));
--- AuthenticationSystem/src/main/java/AuthApp/controller/UserController.java
+++ AuthenticationSystem/src/main/java/AuthApp/controller/UserController.java
@@ -3,7 +3,10 @@ package AuthApp.controller;
 import AuthApp.service.AuthenticationService;
 import AuthApp.service.User;
 import AuthApp.service.UserService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,29 +20,36 @@ public class UserController {
     private UserService userService;
    @Autowired
    private AuthenticationService authenticationService;
+   private static Logger logger = LogManager.getLogger(UserController.class.getName());
 
     private UserController() {
     }
 
     @RequestMapping(value = "name", method = RequestMethod.PATCH)
-    public ResponseEntity<User> updateUserName(@RequestParam String email, @RequestParam String name,
+    public ResponseEntity updateUserName(@RequestParam String email, @RequestParam String name,
                                                @RequestHeader String token) throws IOException {
         if (!InputValidation.isValidName(name)) {
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid name!");
         }
 
-        validateToken(email, token);
+        if (!authenticationService.isValidToken(email, token)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("You must be logged in first!");
+        }
+
         return ResponseEntity.ok(userService.updateUserName(email, name));
     }
 
     @RequestMapping(value = "email", method = RequestMethod.PATCH)
-    public ResponseEntity<User> updateUserEmail(@RequestParam String email, @RequestParam String newEmail,
+    public ResponseEntity updateUserEmail(@RequestParam String email, @RequestParam String newEmail,
                                                 @RequestHeader String token) throws IOException {
         if (!InputValidation.isValidEmail(newEmail)) {
-            throw new IllegalArgumentException(String.format("%s is invalid email!", newEmail));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(String.format("%s is invalid email!", newEmail));
         }
 
-        validateToken(email, token);
+        if (!authenticationService.isValidToken(email, token)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("You must be logged in first!");
+        }
+
         User savedUser = userService.updateUserEmail(email, newEmail);
         authenticationService.updateTokenEmailKey(email, newEmail);
 
@@ -47,27 +57,28 @@ public class UserController {
     }
 
     @RequestMapping(value = "password", method = RequestMethod.PATCH)
-    public ResponseEntity<User> updateUserPassword(@RequestParam String email, @RequestParam String password,
+    public ResponseEntity updateUserPassword(@RequestParam String email, @RequestParam String password,
                                    @RequestHeader String token) throws IOException {
         if (!InputValidation.isValidPassword(password)) {
-            throw new IllegalArgumentException(String.format("%s is invalid password!", password));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(String.format("%s is invalid password!", password));
         }
 
-        validateToken(email, token);
+        if (!authenticationService.isValidToken(email, token)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("You must be logged in first!");
+        }
+
         return ResponseEntity.ok(userService.updateUserPassword(email, password));
     }
 
     @RequestMapping(method = RequestMethod.DELETE)
-    public ResponseEntity<Void> deleteUser(@RequestParam String email, @RequestHeader String token) throws IOException {
-        validateToken(email, token);
+    public ResponseEntity deleteUser(@RequestParam String email, @RequestHeader String token) throws IOException {
+        if (!authenticationService.isValidToken(email, token)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("You must be logged in first!");
+        }
+
         userService.deleteUser(email);
 
-        return ResponseEntity.noContent().build();
-    }
-
-    private void validateToken(String email, String token) throws IOException {
-        if (!authenticationService.isValidToken(email, token)) {
-            throw new AccessDeniedException(String.format("User with email address: %s is not logged in!", email));
-        }
+        return ResponseEntity.ok(String.format("User with email: %s was successfully deleted.", email));
     }
 }
